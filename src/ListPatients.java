0a435f60e0e340dbbef05053a7c9f7811d94da4d
yessineMnejja5 <yessinemnejja5@gmail.com>
2023-11-22 12:03:55 +0100

    first commit

---
+++ src/ListPatients.java
@@ -0,0 +1,34 @@
+public class ListPatients
+    private List&lt;Patient&gt; listP;
+    public ListPatients(){
+        /*à completer*/
+    }
+    public void ajouterPatient(Patient p) {
+        /*à completer*/
+    }
+    public void supprimerPatient(Patient p) {
+
+
+
+        /*à completer*/ (6) (0.5 point)
+    }
+    /* Avec l’api Stream */
+    public boolean rechercherPatient(Patient p){
+        /*à completer*/ (7) (0.5 point)
+    }
+    /* Avec l’api Stream */
+    public boolean rechercherPatient(int cin) {
+        /*à completer*/ (8) } (1 point)
+    /* Avec l’api Stream */
+    public void afficherPatients() {
+        /*à completer*/ (9) (1 point)
+    }
+    /* Avec l’api Stream */
+    public void trierPatientsParNom() {
+        /*à completer */ (10) (1 point)
+    }
+    /* Avec l&#39;api stream */
+    public void PatientSansRedondance(){
+        /*à completer : Afficher la liste des patients sans redondance*/
+    }
+}
